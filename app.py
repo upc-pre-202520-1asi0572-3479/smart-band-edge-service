cd36510201858d369c12432480b6826cf6c89136
Angel Velasquez <pcisavel@upc.edu.pe>
2025-09-29 08:36:36 -0500

    feat(app): add database initialization and test device creation on first request.

--- app.py
+++ app.py
@@ -13,13 +13,18 @@ first_request = True
 
 @app.before_request
 def setup():
+    """
+    Initialize the database and create a test device on the first request.
+    This ensures that the database is set up and a test device is available for authentication.
+    :return:
+    None
+    """
     global first_request
     if first_request:
         first_request = False
         init_db()
         auth_application_service = iam.application.services.AuthApplicationService()
         auth_application_service.get_or_create_test_device()
-
 
 @app.route('/')
 def about_edge_service():  # put application's code here
